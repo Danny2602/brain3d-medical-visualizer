c4ab2cb634c579c00eaf3576af7f089667e679cd
Danny Simba <dannysimba2602@gmail.com>
2026-03-10 19:36:48 -0500

    refresh data

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with default config
 const api = axios.create({
-    baseURL: import.meta.env.API_URL || 'http://127.0.0.1:8000/api/', // Backend API URL
+    baseURL: import.meta.env.VITE_API_URL, // Backend API URL
     withCredentials: false,
     // headers: { 'Content-Type': 'application/json' } // Removed to allow auto-detection for FormData
 });
